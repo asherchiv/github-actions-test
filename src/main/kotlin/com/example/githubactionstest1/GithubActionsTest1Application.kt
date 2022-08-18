982051ae1e90ad602c4f3e19a9bba1e55d811b8b
admin <intern223591@lguplus.co.kr>
2022-08-18 17:59:59 +0900

    test

---
+++ src/main/kotlin/com/example/githubactionstest1/GithubActionsTest1Application.kt
@@ -0,0 +1,11 @@
+package com.example.githubactionstest1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GithubActionsTest1Application
+
+fun main(args: Array<String>) {
+    runApplication<GithubActionsTest1Application>(*args)
+}
